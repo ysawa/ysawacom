32521db07b9420ab35ffd140ec781798ccb1ff2d
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-04-25 23:08:19 +0900

    add: first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ysawacom::Application.config.secret_token = 'ff0280cdbec322832f168d2e7c4c86e13d12e3f85ad3b25d2714aaa8e5b9d042cb877db314f744a5b994a3eb0a42b3b3a9d7c774350ff1ba21737e566b56f19d'
