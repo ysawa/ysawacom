ab73c636d4edc790d1acfb2d45a540b24942b721
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-05-01 01:03:24 +0900

    modify: text

--- app/views/articles/_base.json.rabl
+++ app/views/articles/_base.json.rabl
@@ -1,5 +1,6 @@
 attributes :_id
 attributes :title
 attributes :content
+attributes :description
 node(:created_at) { |model| model.created_at.to_i }
 node(:updated_at) { |model| model.updated_at.to_i }
--- spec/views/articles/index.html.haml_spec.rb
+++ spec/views/articles/index.html.haml_spec.rb
@@ -22,7 +22,6 @@ describe "articles/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr td", text: "Title".to_s, count: 2
-    assert_select "tr td", text: "MyText".to_s, count: 2
     assert_select "tr td", text: "Slug".to_s, count: 2
     assert_select "tr td", text: false.to_s, count: 2
   end
